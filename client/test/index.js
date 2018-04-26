36d4b556f65275fa013c7c9c2e65b26a3c00cfe3
Mark Hernandez <markedcode@gmail.com>
2018-04-25 22:55:47 -0500

    Update dependencies

--- client/test/index.js
+++ client/test/index.js
@@ -1,7 +1,9 @@
 const { expect } = require('chai')
 
 describe('starter', () => {
-  it('should be okay', () => {
+  it('should be okay', done => {
     expect(true).to.equal(true)
+
+    done()
   })
 })
